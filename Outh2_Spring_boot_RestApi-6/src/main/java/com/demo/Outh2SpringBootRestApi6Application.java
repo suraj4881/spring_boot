7cefa7e3959ca2c2ed174b3e2683cb551f4c7255
suraj4881 <suraj4881@gmail.com>
2023-09-17 11:51:37 +0530

    first commit

---
+++ Outh2_Spring_boot_RestApi-6/src/main/java/com/demo/Outh2SpringBootRestApi6Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Outh2SpringBootRestApi6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Outh2SpringBootRestApi6Application.class, args);
+	}
+
+}
---
+++ Outh2_Spring_boot_RestApi-6/src/test/java/com/demo/Outh2SpringBootRestApi6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Outh2SpringBootRestApi6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
